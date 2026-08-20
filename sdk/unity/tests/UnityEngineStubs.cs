18826b8949cfb2ef2f9e20ee1e21db20fb786e75
YUHI <yuhsuke.in@newstory-inc.com>
2026-08-20 10:55:00 +0900

    Add Unity bridge and MAX revenue adapter

---
+++ sdk/unity/tests/UnityEngineStubs.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace UnityEngine
+{
+    public class AndroidJavaObject : IDisposable
+    {
+        public void Dispose() { }
+        public T Call<T>(string method, params object[] args) => default(T);
+        public void Call(string method, params object[] args) { }
+    }
+    public class AndroidJavaClass : AndroidJavaObject
+    {
+        public AndroidJavaClass(string name) { }
+        public T GetStatic<T>(string name) => default(T);
+        public void CallStatic(string method, params object[] args) { }
+    }
+    public class AndroidJavaProxy
+    {
+        protected AndroidJavaProxy(string interfaceName) { }
+    }
+    public static class AndroidJNI
+    {
+        public static int AttachCurrentThread() => 0;
+    }
+}
